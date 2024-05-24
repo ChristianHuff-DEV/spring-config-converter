a65683af160cf4cc7d2e856f06ac2b781f875e4f
Christian Huff <christian.huff.dev@gmail.com>
2024-05-24 06:57:35 +0100

    Initialize Spring Boot project

---
+++ spring-config-converter/src/main/java/com/example/springconfigconverter/SpringConfigConverterApplication.java
@@ -0,0 +1,13 @@
+package com.example.springconfigconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigConverterApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringConfigConverterApplication.class, args);
+  }
+
+}
---
+++ spring-config-converter/src/test/java/com/example/springconfigconverter/SpringConfigConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springconfigconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigConverterApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
